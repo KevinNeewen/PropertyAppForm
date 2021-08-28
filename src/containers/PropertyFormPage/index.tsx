c320e543fd87ba366807c185de734d74b2408f1b
Kevin Nguyen <kevin.nguyen1996@gmail.com>
2021-08-28 13:39:16 +1000

    Stepper style and animation

--- src/containers/PropertyFormPage/index.tsx
+++ src/containers/PropertyFormPage/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Page from '../../components/Page';
 import { PropertyFormStepsEnum, PropertyFormStepsToDescriptionMap } from './formSteps';
-import { Stepper, Step, StepLabel, StepContent, Grid } from '@material-ui/core';
+import { Stepper, Step, StepLabel, StepContent, Grid, StepConnector, Container } from '@material-ui/core';
 import Button from '../../components/Button';
 import Header from '../../components/Header';
 import { WithStyles, withStyles } from '@material-ui/styles';
@@ -31,26 +31,52 @@ const PropertyFormPage = (props: MyProps) => {
 
     const steps = getSteps();
 
+    const renderStyledStepConnector = () => {
+        return <StepConnector classes={{ root: classes.stepConnectorRoot }} />;
+    };
+
     return (
         <Page appBarContent={<div></div>}>
             <Grid container direction="row">
-                <Grid item xs={2}>
-                    <Stepper activeStep={activeStep} orientation="vertical">
-                        {steps.map((label, index) => (
-                            <Step key={index}>
-                                <StepLabel>{label}</StepLabel>
-                            </Step>
-                        ))}
-                    </Stepper>
+                <Grid item xs={3}>
+                    <Container classes={{ root: classes.stepperContainer }} maxWidth="md">
+                        <Stepper //
+                            classes={{ root: classes.stepperBar }}
+                            activeStep={activeStep}
+                            orientation="vertical"
+                            connector={renderStyledStepConnector()}
+                        >
+                            {steps.map((label, index) => (
+                                <Step key={index}>
+                                    <StepLabel
+                                        classes={{
+                                            root: classes.stepLabelRoot,
+                                            vertical: classes.stepLabelVertical,
+                                        }}
+                                    >
+                                        {label}
+                                    </StepLabel>
+                                </Step>
+                            ))}
+                        </Stepper>
+                    </Container>
                 </Grid>
                 <Grid item xs={9}>
                     <Header variant="h1">{PropertyFormStepsToDescriptionMap[activeStep]}</Header>
                     <div className={classes.form}></div>
                     <div>
-                        <Button invisible disabled={activeStep === 0} onClick={handlePrevStep}>
+                        <Button //
+                            invisible
+                            disabled={activeStep === 0}
+                            onClick={handlePrevStep}
+                        >
                             Back
                         </Button>
-                        <Button onClick={handleNextStep}>{activeStep === steps.length - 1 ? 'Submit' : 'Next'}</Button>
+                        <Button //
+                            onClick={handleNextStep}
+                        >
+                            {activeStep === steps.length - 1 ? 'Submit' : 'Next'}
+                        </Button>
                     </div>
                 </Grid>
             </Grid>
--- src/containers/PropertyFormPage/styles.ts
+++ src/containers/PropertyFormPage/styles.ts
@@ -1,5 +1,6 @@
 import { Theme } from '@material-ui/core';
 import { createStyles } from '@material-ui/styles';
+import { relative } from 'path';
 
 const styles = (theme: Theme) =>
     createStyles({
@@ -7,6 +8,76 @@ const styles = (theme: Theme) =>
             height: '50rem',
             width: '40rem',
             border: '2px solid black',
+        },
+        stepperBar: {
+            backgroundColor: 'inherit',
+            position: 'absolute',
+            top: '40%',
+            left: '50%',
+            transform: 'translate(-50%,-50%)',
+        },
+        stepperContainer: {
+            height: '100%',
+            position: 'relative',
+        },
+        stepConnectorRoot: {
+            padding: 0,
+
+            '& .MuiStepConnector-line': {
+                float: 'right',
+                marginTop: '.3rem',
+                marginBottom: '.3rem',
+                marginRight: '1.3rem',
+                border: `1px solid #C4CCF8`,
+            },
+        },
+        stepLabelVertical: {
+            flexDirection: 'row-reverse',
+        },
+        stepLabelRoot: {
+            '& .MuiStepLabel': {
+                color: '#C4CCF8',
+                '&-completed': {
+                    fontWeight: 600,
+                    color: '#232D42',
+                },
+                '&-active': {
+                    fontWeight: 600,
+                    color: '#232D42',
+                },
+                '&-labelContainer': {
+                    width: 'auto',
+                    '& span': {
+                        fontSize: '.8rem',
+                    },
+                },
+                '&-iconContainer': {
+                    '& svg': {
+                        fontSize: '1.3rem',
+                        marginLeft: '.6rem',
+                        fill: 'transparent',
+                        border: `2px solid #C4CCF8`,
+                        borderRadius: '5rem',
+                        '& text': {
+                            display: 'none',
+                        },
+                    },
+                    '& .MuiStepIcon': {
+                        '&-active': {
+                            fill: theme.palette.primary.main,
+                            padding: '1.3px',
+                            fontSize: '1.5rem',
+                            border: `1px solid ${theme.palette.primary.main}`,
+                        },
+                        '&-completed': {
+                            fill: theme.palette.primary.main,
+                            border: 'none',
+                            fontSize: '1.4rem',
+                            // border: `2px solid ${theme.palette.primary.main}`,
+                        },
+                    },
+                },
+            },
         },
     });
 
